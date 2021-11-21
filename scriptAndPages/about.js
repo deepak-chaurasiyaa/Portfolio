b5793ae18cc25ab5864a53cb71a9aa750367fa9f
Deepak Chaurasiya <dchaurasiya8589@gmail.com>
2021-11-21 16:51:04 +0530

    portfolio

---
+++ scriptAndPages/about.js
@@ -0,0 +1,35 @@
+let about = document.getElementById("about")
+about.innerHTML = `
+<section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left shadow">
+                    <img src="images/mains-modified.png" alt=""
+                        style="border-radius: 300px; background-color: rgba(146, 139, 139, 0.068);">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Deepak and I'm a <span class="typing-2"></span></div>
+                    <p>Aspiring Full StackWeb Developer, a passionate coder, and capable of writing efficient code using
+                        MERN Stack. Ambitious to work and collaborate with other dedicated and enterprising developers.
+                        <br>
+                        Eager to take up a responsible role within an exciting and inspiring organization, and deliver
+                        products that offer a great user experience.
+                    </p>
+                    <div class="mediaLinksAbout">
+                        <a href="https://www.linkedin.com/in/deepak-chaurasiya/" target="blank">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                        <a href="https://github.com/deepak-chaurasiyaa" target="blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </div>
+                    <p class="iconsAboutSection"> <i class="fas fa-phone"></i> +91-9792401189</p>
+                    <p class="iconsAboutSection"><i class="fas fa-envelope"></i> dchaurasiya8589@gmail.com</p>
+                    <a href="https://drive.google.com/file/d/1aghg-79Fv1XeB-touM8tIwuEomzOtQO8/view?usp=sharing"
+                        target="blank" class="resumeToDrive">Resume</a>
+                </div>
+            </div>
+        </div>
+    </section>
+`;
---
+++ scriptAndPages/projects.js
@@ -0,0 +1,82 @@
+let projectsSection = document.getElementById("mainDivProjectsSection");
+projectsSection.innerHTML = `
+<!-- EVehicle project -->
+<div class="eachProjectDiv EVehicle">
+                <p class="ProjectName ">E-Vehicle Management</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/ev_managements.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                        <p>E-Vehicle management project is a platform to provide road-side assistant to the electric vehicle user</a></p>
+                       
+                        <br />
+                        <p>
+                            We a team of 6 have build E-Vehicle management project in 5 days, where i have
+                            developed the "Gallery", "Login and Signup" and the "Account" sections of
+                            website.
+                        </p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-react"></i> React</p>
+                            <p><svg width="15" height="15" viewBox="0 0 256 208" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet"><path d="M15.624 35.012v87.53l15.058-8.471V43.482l-15.058-8.47z" fill="white"/><path d="M.565 114.07V8.66l89.411 50.823v17.883L15.624 35.012v87.53L.564 114.07z" fill="white"/><path d="M181.27 8.659V114.07l-60.235 33.882-15.059-9.412 60.236-33.882V34.07L89.976 77.365V59.482L181.271 8.66z" fill="white"/><path d="M166.212.188L90.918 42.541 15.624.188.564 8.658l89.412 50.824L181.271 8.66 166.21.189z" fill="white"/><path d="M151.153 42.541v53.647l15.059 8.47V34.072l-15.06 8.47z" fill="white"/><path d="M75.859 138.541l75.294-42.353 15.059 8.47-60.236 33.883 59.295 33.883 60.235-33.883 15.059 8.47-75.294 42.354-89.412-50.824z" fill="white"/><path d="M75.859 138.541v17.883l89.412 50.823v-17.882L75.859 138.54zM240.565 147.012V94.306l-15.06-8.47v52.705l15.06 8.47z" fill="white"/><path d="M165.27 189.365l75.295-42.353V94.306l15.059-8.47v70.588l-90.353 50.823v-17.882z" fill="white"/><path d="M255.624 85.835l-15.06 8.47-15.058-8.47 15.059-8.47 15.059 8.47z" fill="white"/><path d="M240.565 77.365V59.482l15.059-8.47v17.882l-15.06 8.47z" fill="white"/><path d="M240.565 59.482v17.883l-15.06-8.47V51.011l15.06 8.47z" fill="white"/><path d="M255.624 51.012l-15.06 8.47-15.058-8.47 15.059-8.47 15.059 8.47z" fill="white"/></svg> Material UI</p>
+                            <p> <i class="fab fa-node-js"></i> NodeJs</p>
+                            <p> <i class="fab fa-node-js"></i> ExpressJs</p>
+                            <p> <i class="fas fa-database"></i> MongoDb</p>
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/deepak-chaurasiyaa/ev-front-end-react-app" target="blank"><button>
+                                    Github</button></a>
+                            <a href="" target="blank"><button>Demo</button></a>
+                            <a href="https://medium.com/@dchaurasiya8589/hello-readers-this-is-deepak-from-team-electric-908c2b43deab/" target="blank"><button>Blog</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- lybrate project -->
+            <div class="eachProjectDiv">
+                <p class="ProjectName">Lybrate Clone</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/lybrate.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                        <p>Developed a lookalike of <a href="https://www.lybrate.com/"
+                                style="color: #FFF; text-decoration: underline;" target="blank">Lybrate</a></p>
+                        <p>A web application to find the best doctors in India and consult online with
+                            them for any kind of medical ailments.</p>
+                        <br />
+                        <p> Along withy 3 other members, build lybrate clone in 4 days, where
+                            i have developed the "consult online" and the "consult now" page of website.
+                        </p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <br>
+                        <div class="techStackProject">
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            <p> <i class="fab fa-js-square"></i> EJS </p>
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-node-js"></i> NodeJs</p>
+                            <p> <i class="fab fa-node-js"></i> ExpressJs</p>
+                            <p> <i class="fas fa-database"></i> MongoDb</p>
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/deepak-chaurasiyaa/lybrate-Clone-express-"
+                                target="blank"><button>
+                                    Github</button></a>
+                            <a href="https://lybrate.herokuapp.com/" target="blank"><button>Demo</button></a>
+                            <a href="https://lybrate.herokuapp.com/" target="blank"><button>Blog</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+`;
